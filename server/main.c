c79cdbb30803526d75b9572b7f71ee58dc1e4102
vailla_y <vailla_y@59f4a81d-3463-4c2a-be09-1ad07fac521b>
2012-06-05 10:28:10 +0000

    add main
    git-svn-id: svn+ssh://koala-rendus.epitech.net/zappy-2015-2014s-haulot_a@3 59f4a81d-3463-4c2a-be09-1ad07fac521b

---
+++ server/main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for  in /home/vailla_y/Projet/zappy/zappy-2015-2014s-haulot_a/server
+** 
+** Made by yann vaillant
+** Login   <vailla_y@epitech.net>
+** 
+** Started on  Tue Jun  5 12:26:29 2012 yann vaillant
+** Last update Tue Jun  5 12:26:30 2012 yann vaillant
+*/
+
+int main(int ac, char **av)
+{
+  
+
+}
