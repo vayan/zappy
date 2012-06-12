8b762e043cc9623251bd1e303da0178f7b37f117
haulot_a <haulot_a@59f4a81d-3463-4c2a-be09-1ad07fac521b>
2012-06-12 12:53:18 +0000

    client forkeur faite gaffes...
    git-svn-id: svn+ssh://koala-rendus.epitech.net/zappy-2015-2014s-haulot_a@30 59f4a81d-3463-4c2a-be09-1ad07fac521b

---
+++ client/include/PlayerCreator.hh
@@ -0,0 +1,24 @@
+//
+// PlayerCreator.hh for PlCreator in /home/haulot_a//zappy-2015-2014s-haulot_a/client
+// 
+// Made by alexandre haulotte
+// Login   <haulot_a@epitech.net>
+// 
+// Started on  Tue Jun 12 10:42:26 2012 alexandre haulotte
+// Last update Tue Jun 12 14:21:04 2012 alexandre haulotte
+//
+
+#ifndef	__PLAYERCREATOR_HH__
+#define	__PLAYERCREATOR_HH__
+
+#include	<vector>
+
+class PlayerCreator
+{
+public:
+  PlayerCreator();
+  ~PlayerCreator();
+  int	create(std::vector<int> vec);
+};
+
+#endif
