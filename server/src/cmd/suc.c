8d83403bfc209c0e563d3a4a59555f49be2f6d99
lyvet_r <lyvet_r@59f4a81d-3463-4c2a-be09-1ad07fac521b>
2012-06-15 13:36:24 +0000

    Add suc
    git-svn-id: svn+ssh://koala-rendus.epitech.net/zappy-2015-2014s-haulot_a@63 59f4a81d-3463-4c2a-be09-1ad07fac521b

--- server/src/cmd/suc.c
+++ server/src/cmd/suc.c
@@ -5,14 +5,17 @@
 ** Login   <carlie_a@epitech.net>
 ** 
 ** Started on  Tue Jun 12 11:18:04 2012 anatole carlier
-** Last update Thu Jun 14 12:36:34 2012 randy lyvet
+** Last update Fri Jun 15 15:36:05 2012 randy lyvet
 */
 
-#include <stdio.h>
+#include 	<stdio.h>
 
-int	suc(char **tab)
+#include	"network.h"
+
+int	suc(char **tab, t_client *client)
 {
   tab = tab;
-  printf("suc\n");
+
+  broadcast_to_one_client("suc\n", client);
   return (0);
 }
--- server/src/command_parser.c
+++ server/src/command_parser.c
@@ -5,7 +5,7 @@
 ** Login   <lyvet_r@epitech.net>
 ** 
 ** Started on  Tue Jun 12 11:37:00 2012 randy lyvet
-** Last update Fri Jun 15 15:15:54 2012 randy lyvet
+** Last update Fri Jun 15 15:32:26 2012 randy lyvet
 */
 
 #include	<string.h>
@@ -19,14 +19,20 @@ int			command_parser(t_option *tab, char *command, t_client *client)
 {
   int           j;
   char		**command_type;
+  int		ret;
+  int 		g;
 
   command_type = my_str_to_wordtab(command, ' ');
   j = -1;
   while (++j < TAB_SIZE)
     if (strcmp(tab[j].option, command_type[0]) == 0)
-      return (tab[j].op_func(command_type, client));
-  j = -1;
-  while (command_type[++j] != 0)
-    free(command_type[j]);
-  free(command_type);
+      {
+	ret = tab[j].op_func(command_type, client);
+	g = -1;
+	while (command_type[++j] != 0)
+	  free(command_type[j]);
+	free(command_type);
+	return (ret);
+      }
+  return (tab[24].op_func(command_type, client));
 }
