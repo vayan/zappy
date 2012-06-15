ae99857b906936bd120aa9dfaccbb4df4ebac5c9
carlie_a <carlie_a@59f4a81d-3463-4c2a-be09-1ad07fac521b>
2012-06-15 14:53:32 +0000

    git-svn-id: svn+ssh://koala-rendus.epitech.net/zappy-2015-2014s-haulot_a@75 59f4a81d-3463-4c2a-be09-1ad07fac521b

---
+++ server/include/merge.h
@@ -0,0 +1,16 @@
+/*
+** merge.h for zappy in /home/carlie_a//zappy-2015-2014s-haulot_a/server/src/cmd
+** 
+** Made by anatole carlier
+** Login   <carlie_a@epitech.net>
+** 
+** Started on  Fri Jun 15 13:44:39 2012 anatole carlier
+** Last update Fri Jun 15 13:48:59 2012 anatole carlier
+*/
+
+#ifndef		__MERGE_H__
+# define	__MERGE_H__
+
+char	*my_merge(char *path, char *name);
+
+#endif /* !__MERGE_H__ */
