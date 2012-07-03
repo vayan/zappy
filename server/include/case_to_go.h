3a954d1503c0a0eaa86486854e0a40430a3e5d58
lyvet_r <lyvet_r@59f4a81d-3463-4c2a-be09-1ad07fac521b>
2012-07-03 09:11:34 +0000

    correction case_to_go.h
    git-svn-id: svn+ssh://koala-rendus.epitech.net/zappy-2015-2014s-haulot_a@154 59f4a81d-3463-4c2a-be09-1ad07fac521b

--- server/include/case_to_go.h
+++ server/include/case_to_go.h
@@ -5,13 +5,12 @@
 ** Login   <lyvet_r@epitech.net>
 ** 
 ** Started on  Fri Jun 22 10:48:09 2012 randy lyvet
-** Last update Fri Jun 29 14:31:05 2012 randy lyvet
+** Last update Tue Jul  3 13:11:11 2012 randy lyvet
 */
 
 #ifndef		CASE_TO_GO_H_
 #define		CASE_TO_GO_H_
 
-int go_case(int x, int y, int dir);
 int go_right(int x, int y);
 int go_bot(int x, int y);
 int go_left(int x, int y);
