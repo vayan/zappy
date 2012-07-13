1d66cc15a2c7453a41ed568e491a617b67010850
vailla_y <vailla_y@59f4a81d-3463-4c2a-be09-1ad07fac521b>
2012-07-13 09:44:42 +0000

    fix
    git-svn-id: svn+ssh://koala-rendus.epitech.net/zappy-2015-2014s-haulot_a@234 59f4a81d-3463-4c2a-be09-1ad07fac521b

--- server/src/incant.c
+++ server/src/incant.c
@@ -55,10 +55,15 @@ int can_elev(t_client *cl)
   return (1);
 }
 
-int	do_elev(int *req, t_client *cl, t_map_case *cas)
+int	do_elev(int *req, t_client *cl, t_map_case *cas, int prec)
 {
-  cas = cas;
-  req = req;
+  if (prec == 1 || (int)req[Linemate] > (int)cas->rsrc[Linemate] ||
+    (int)req[Deraumere] > (int)cas->rsrc[Deraumere] ||
+    (int)req[Sibur] > (int)cas->rsrc[Sibur] ||
+    (int)req[Mendiane] > (int)cas->rsrc[Mendiane] ||
+    (int)req[Phiras] > (int)cas->rsrc[Phiras] ||
+    (int)req[Thystame] > (int)cas->rsrc[Thystame])
+    return (0);
   cl->level++;
   if (cl->level == 8)
     cl->teams->nb_lvl_max++;
@@ -71,20 +76,28 @@ int		do_incant(t_client *cl)
   t_pl_case	*all_cl_case;
   int		*req;
   char		*msg;
+  int nb_pl;
+  int dead;
 
+  dead = 0;
   msg = xmalloc (200 * sizeof(char));
   req = xmalloc (10 * sizeof(int));
   map = get_map(NULL);
+  memset(req, 0, 10);
+  fill_tab_req(req, cl->level);
   all_cl_case = (map[cl->x][cl->y])->client;
+  nb_pl = count_pl_on_case(cl->level, all_cl_case);
+  if (nb_pl < req[0])
+    dead = 1;
   while (all_cl_case)
-  {  
-    memset(req, 0, 10);
-    fill_tab_req(req, all_cl_case->client->level);
+  {     
     pie(all_cl_case->client, 
-      do_elev(req, all_cl_case->client, (map[all_cl_case->client->x][all_cl_case->client->y])));
+      do_elev(req, all_cl_case->client, 
+        (map[all_cl_case->client->x][all_cl_case->client->y]), dead));
     plv(NULL, all_cl_case->client);
     bct(NULL, all_cl_case->client);
     sprintf(msg, "niveau actuel : %d\n", all_cl_case->client->level);
+    usleep(1);
     broadcast_to_one_client(msg, all_cl_case->client);
     all_cl_case->client->stm->in_use = -1;
     all_cl_case = all_cl_case->next;
@@ -102,13 +115,7 @@ int		start_incant_all_on_case(t_client *cl)
   map = get_map(NULL);
   all_cl_case = (map[cl->x][cl->y])->client;
   pic(cl, (map[cl->x][cl->y]));
-  while (all_cl_case)
-  {
-    broadcast_to_one_client("elevation en cours\n", all_cl_case->client);
-    if ((all_cl_case->client != cl))
-      all_cl_case->client->stm->in_use = -21;
-    all_cl_case = all_cl_case->next;
-  }
+  broadcast_to_one_client("elevation en cours\n", cl);
   return (0);
 }
 
@@ -122,8 +129,7 @@ int		incant(t_client *cl, int first)
   {
     if (can_elev(cl) == 0)
       return (0);
-    if (first == 1)
-      start_incant_all_on_case(cl);
+    start_incant_all_on_case(cl);
     cl->stm->in_use = Incant;
     start_timer(cl->stm);
     return (1);
